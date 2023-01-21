1a3ef5f5703486a667fbc1497e05c8d95b7a3b96
jeromyzhang <zjdx1998@gmail.com>
2023-01-20 19:09:34 -0800

    Add build.sh/bat for linux/mac and windows.

--- build.bat
+++ build.bat
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-# make sure you hava installed xQuartz for mac or XServer for linux.
-xhost +
+:: make sure you have installed XMing for windows, and run this script in administrator mode.
+xming -ac
 git clone git@github.com:Spring23BuildingGameEngines/monorepo-zjdx1998.git
 cd monorepo-zjdx1998/Assignment1_cpp/part2
 curl -o Dockerfile https://raw.githubusercontent.com/zjdx1998/sdl2docker/main/Dockerfile
-sudo docker build -t sdl_project .
+docker build -t sdl_project .
 docker run -it --privileged sdl_project "$@"
